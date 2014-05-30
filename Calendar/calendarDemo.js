aa0cca44d023e99c7a667120f815a2c7f6cf5e9c
Justin Tafoya <tafoya9099@gmail.com>
2014-05-29 20:00:05 -0600

    Calendar maps updated

--- Calendar/calendarDemo.js
+++ Calendar/calendarDemo.js
@@ -8,53 +8,56 @@ function CalendarCtrl($scope) {
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
-    
-    $scope.changeTo = 'Hungarian';
+
+    $scope.clickedRide = '';
+
     /* event source that pulls from google.com */
     $scope.eventSource = {
             url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
             className: 'gcal-event',           // an option!
-            currentTimezone: 'America/Chicago' // an option!
+            currentTimezone: 'America/Denver' // an option!
     };
     /* event source that contains custom events on the scope */
     $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+      // {title: 'All Day Event',start: new Date(y, m, 1)},
+      {title: 'Portland',start: new Date(y, m, 1 - 0),end: new Date(y, m, 6 - 0)},
+      {title: 'Denver',start: new Date(y, m, 15 - 0),end: new Date(y, m, 22 - 0)},
+      {title: 'Phoenix',start: new Date(y, 6, 15 - 0),end: new Date(y, 6, 22 - 0)},
+      // {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+      // {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+      // {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://yahoo.com/'}//Page link
     ];
     /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, callback) {
       var s = new Date(start).getTime() / 1000;
       var e = new Date(end).getTime() / 1000;
       var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      // var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
       callback(events);
     };
 
-    $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
-    };
+    // $scope.calEventsExt = {
+    //    color: '#f00',
+    //    textColor: 'yellow',
+    //    events: [ 
+    //       {type:'party',title: 'Portland',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+    //       {type:'party',title: 'Denver',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+    //       {type:'party',title: 'Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://yahoo.com/'}
+    //     ]
+    // };
     /* alert on eventClick */
     $scope.alertOnEventClick = function( event, allDay, jsEvent, view ){
-        $scope.alertMessage = (event.title + ' was clicked ');
+        $scope.alertMessage = (event.title + ' trip');
+        $scope.clickedRide = event.title;
     };
-    /* alert on Drop */
-     $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
-       $scope.alertMessage = ('Event Droped to make dayDelta ' + dayDelta);
-    };
-    /* alert on Resize */
-    $scope.alertOnResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){
-       $scope.alertMessage = ('Event Resized to make dayDelta ' + minuteDelta);
-    };
+    // /* alert on Drop */
+    //  $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
+    //    $scope.alertMessage = ('Event Droped to make dayDelta ' + dayDelta);
+    // };
+    // /* alert on Resize */
+    // $scope.alertOnResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){
+    //    $scope.alertMessage = ('Event Resized to make dayDelta ' + minuteDelta);
+    // };
     /* add and removes an event source of choice */
     $scope.addRemoveEventSource = function(sources,source) {
       var canAdd = 0;
@@ -107,21 +110,26 @@ function CalendarCtrl($scope) {
       }
     };
 
-    $scope.changeLang = function() {
-      if($scope.changeTo === 'Hungarian'){
-        $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
-        $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
-        $scope.changeTo= 'English';
-      } else {
-        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        $scope.changeTo = 'Hungarian';
-      }
-    };
+    // $scope.changeLang = function() {
+    //   if($scope.changeTo === 'Hungarian'){
+    //     $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+    //     $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
+    //     $scope.changeTo= 'English';
+    //   } else {
+    //     $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //     $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    //     $scope.changeTo = 'Hungarian';
+    //   }
+    // };
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
 }
+
+
+
+
+
 
 
 
--- Calendar/map.js
+++ Calendar/map.js
@@ -1,143 +1,227 @@
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(41.222725, -111.973704);
+  var mapOptions = {
+    zoom: 15,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* MAP */
 
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+// jQuery to collapse the navbar on scroll
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+// $(function() {
+//     $('.page-scroll a').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
 
-//http://snazzymaps.com/
-var myOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(53.385873, -1.471471),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: true,
-    styles: [{
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }]
-    }, {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 20
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 29
-        }, {
-            "weight": 0.2
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 18
-        }]
-    }, {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 16
-        }]
-    }, {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 21
-        }]
-    }, {
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-            "visibility": "on"
-        }, {
-            "color": "#000000"
-        }, {
-            "lightness": 16
-        }]
-    }, {
-        "elementType": "labels.text.fill",
-        "stylers": [{
-            "saturation": 36
-        }, {
-            "color": "#000000"
-        }, {
-            "lightness": 40
-        }]
-    }, {
-        "elementType": "labels.icon",
-        "stylers": [{
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "transit",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 19
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 20
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#000000"
-        }, {
-            "lightness": 17
-        }, {
-            "weight": 1.2
-        }]
-    }]
-};
+// function initialize() {
+// var myOptions = {
+//     zoom: 15,
+//     center: new google.maps.LatLng(53.385873, -1.471471),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     disableDefaultUI: true,
+    
+//     styles: [{
+//         "featureType": "water",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "blue"
+//         }, {
+//             "lightness": 17
+//         }]
+//     }, {
+//         "featureType": "landscape",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 20
+//         }]
+//     }, {
+//         "featureType": "road.highway",
+//         "elementType": "geometry.fill",
+//         "stylers": [{
+//             "color": "#FFFFFF"
+//         }, {
+//             "lightness": 17
+//         }]
+//     }, {
+//         "featureType": "road.highway",
+//         "elementType": "geometry.stroke",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 29
+//         }, {
+//             "weight": 0.2
+//         }]
+//     }, {
+//         "featureType": "road.arterial",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 18
+//         }]
+//     }, {
+//         "featureType": "road.local",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 16
+//         }]
+//     }, {
+//         "featureType": "poi",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 21
+//         }]
+//     }, {
+//         "elementType": "labels.text.stroke",
+//         "stylers": [{
+//             "visibility": "on"
+//         }, {
+//             "color": "#000000"
+//         }, {
+//             "lightness": 16
+//         }]
+//     }, {
+//         "elementType": "labels.text.fill",
+//         "stylers": [{
+//             "saturation": 36
+//         }, {
+//             "color": "#000000"
+//         }, {
+//             "lightness": 40
+//         }]
+//     }, {
+//         "elementType": "labels.icon",
+//         "stylers": [{
+//             "visibility": "off"
+//         }]
+//     }, {
+//         "featureType": "transit",
+//         "elementType": "geometry",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 19
+//         }]
+//     }, {
+//         "featureType": "administrative",
+//         "elementType": "geometry.fill",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 20
+//         }]
+//     }, {
+//         "featureType": "administrative",
+//         "elementType": "geometry.stroke",
+//         "stylers": [{
+//             "color": "#000000"
+//         }, {
+//             "lightness": 17
+//         }, {
+//             "weight": 1.2
+//         }]
+//     }]
+// };
 
-var map = new google.maps.Map(document.getElementById('map'), myOptions);
+// var map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+//   var marker = new google.maps.Marker({
+//       position: myLatlng,
+//       map: map,
+//       title: 'Hello World!'
+//   });
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
 
+
+// function initCall() {
+//   console.log('Google maps api initialized.');
+//   angular.bootstrap(document.getElementById('map'), ['doc.ui-map']);
+// }
+ 
+// app.controller('MapCtrl', ['$scope', function($scope) {
+ 
+// $scope.myMarkers = [];
+ 
+// $scope.mapOptions = {
+//   center: new google.maps.LatLng(35.784, -78.670),
+//   zoom: 15,
+//   mapTypeId: google.maps.MapTypeId.ROADMAP
+// };
+ 
+// $scope.addMarker = function($event, $params) {
+//   $scope.myMarkers.push(new google.maps.Marker({
+//     map: $scope.myMap,
+//     position: $params[0].latLng
+//   }));
+// };
+ 
+// $scope.setZoomMessage = function(zoom) {
+//   $scope.zoomMessage = 'You just zoomed to '+zoom+'!';
+//   console.log(zoom,'zoomed')
+// };
+ 
+// $scope.openMarkerInfo = function(marker) {
+//   $scope.currentMarker = marker;
+//   $scope.currentMarkerLat = marker.getPosition().lat();
+//   $scope.currentMarkerLng = marker.getPosition().lng();
+//   $scope.myInfoWindow.open($scope.myMap, marker);
+// };
+ 
+// $scope.setMarkerPosition = function(marker, lat, lng) {
+//   marker.setPosition(new google.maps.LatLng(lat, lng));
+// };
+// }]);
